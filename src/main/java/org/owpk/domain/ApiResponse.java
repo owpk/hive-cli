6f5abb9baa850b92514b802f41c4d436efd58c6d
owpk <vzvz4@mail.ru>
2021-03-28 20:30:19 +0800

    fix: ApiResponse @Introspected annotation added

--- src/main/java/org/owpk/domain/ApiResponse.java
+++ src/main/java/org/owpk/domain/ApiResponse.java
@@ -1,6 +1,7 @@
 package org.owpk.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.micronaut.core.annotation.Introspected;
 import lombok.Data;
 
 import java.util.Collections;
@@ -8,6 +9,7 @@ import java.util.List;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Introspected
 public class ApiResponse<T> {
     private List<T> data = Collections.emptyList();
 }
